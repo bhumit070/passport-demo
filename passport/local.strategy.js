3e0ecb2baadf42cd56cc8cded4b0cceb1ff613cf
bhumit070 <ganatrabhoomit070@gmail.com>
2022-10-01 18:38:22 +0530

    a way to add extra fields in username. this is a patch need to find a proper solution for this

--- passport/local.strategy.js
+++ passport/local.strategy.js
@@ -4,12 +4,21 @@ const response_helpers = require('../helpers/response')
 
 async function validate_local_strategy(username, password, callback) {
 	try {
+		// I am not getting any wat to find extra properties in the username property.
+		// so here is the patch for that 😅.
+		// we can send JSON like below from frontend and then we can parse it in backend.
+		//{
+		//	"username": "{ \"username\": \"bhoomit\" }",
+		//		"password": "bhoomit"
+		//}
+		// and after parsing username we can have extra fields like username,phone_number, etc.
+		username = JSON.parse(username)
 
 		if (!username || !password) {
 			return callback(response_helpers.generate_response({ error: true, message: 'username & password are required.', status: 400 }), null)
 		}
 
-		const user = await user_model.findOne({ username }).lean()
+		const user = await user_model.findOne({ username: username.username }).lean()
 
 		if (!user) {
 			return callback(response_helpers.generate_response({ error: true, status: 404, message: 'User not found.' }), null)
